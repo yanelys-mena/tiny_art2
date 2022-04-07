06a9ee3aa0227ce6bd9dbb2f9191c8ac297a6cb9
yanelys-mena <yanelyshm@outlook.com>
2022-04-07 15:03:11 -0400

    base uri

--- contracts/tinyNFT.sol
+++ contracts/tinyNFT.sol
@@ -16,9 +16,25 @@ contract TinyArt is ERC721, ERC721URIStorage, Ownable {
     Counters.counter private _tokenIdCounter;
 
     // unique 8 bit unsigned integer
+    // this is creating an object as "string": 8bitnumber
     mapping(string => uint8) existingURIs;
 
     // create a URI that will give direct access to the bucket and where the NFT is located.
     // ERC721 takes name of contract, and token.
     constructor() ERC721("TinyArt", "TNY") {}
-};
+
+    // this function is an internal can only run once with this function
+    //a pure function will not have access to anything that can be mutable in the future. Declare what the function is taking in and then it takes the type of memory
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://";
+    }
+
+    // address to is the person who is getting the token.Everyone who utilizes the ehterium network has a unique address. The address/string are data types
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+        eixstingURIs[uri] = 1;
+    }
+}
