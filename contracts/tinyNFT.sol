f41e3105984b8b67b124f66cdb5b9c0fd1709817
yanelys-mena <yanelyshm@outlook.com>
2022-04-07 14:20:18 -0400

    imports

--- contracts/tinyNFT.sol
+++ contracts/tinyNFT.sol
@@ -1,2 +1,7 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
